ea75ea7cf2789614cad907d2aa5a04b2f317d4ed
Smile-Khan <ismailkhan.pathan19@gmail.com>
2025-06-06 18:24:10 +0530

    Update file upload paths and related changes

--- backend/src/main/java/com/employee/app/controller/EmployeeController.java
+++ backend/src/main/java/com/employee/app/controller/EmployeeController.java
@@ -10,7 +10,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.employee.app.dto.EmployeeSearchCriteria;
@@ -28,7 +36,8 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @Value("${file.upload-dir}")
+    
+    @Value("${file.upload-dir:/data/uploads/}")
     private String uploadsDir;
 
     // Add new employee
